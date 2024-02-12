827d12645085013897571b5d4e2e871098a0e186
Smugpng <zikadika2@gmail.com>
2024-02-11 21:28:17 -0500

    Build Changes
    //ed packet manager and open cover in player movent and field of view, also turned the ghost movement and nav scripts off for the time being

--- Spoooookyhouse/Assets/Scripts/FieldOfView.cs
+++ Spoooookyhouse/Assets/Scripts/FieldOfView.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using OpenCover.Framework.Model;
+//using OpenCover.Framework.Model;
 
 [RequireComponent(typeof(PatrollingScript))]
 public class FieldOfView : MonoBehaviour //this script is what makes the ghosts vision cone
--- Spoooookyhouse/Assets/Scripts/PlayerMovement.cs
+++ Spoooookyhouse/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
+
 
 public class PlayerMovement : MonoBehaviour
 {
